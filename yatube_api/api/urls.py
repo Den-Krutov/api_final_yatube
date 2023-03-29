43dad95eeefe0eeac5b9b5cf20769a7b109c800a
Daniil Fedin <nniiyu@yandex.ru>
2023-03-29 13:31:01 +0300

    Added authenticated jwt and access to posts for api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
+    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,17 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Post
+from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related('author')
+    serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
